cd884ce8ba1ffd32851fc71d149bef81fcc27044
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-01 20:41:37 +0700

    update

--- chatbot/config.py
+++ chatbot/config.py
@@ -3,7 +3,8 @@ import os
 # --- Cấu hình Mô hình ---
 embedding_model_name = 'truro7/vn-law-embedding' 
 reranking_model_name = 'namdp-ptit/ViRanker' 
-gemini_model_name = 'gemini-2.5-flash-preview-04-17' 
+# gemini_model_name = 'gemini-2.5-flash-preview-04-17' 
+gemini_model_name = 'gemini-2.0-flash'
 
 # --- Cấu hình Đường dẫn ---
 KAGGLE_INPUT_PATH = '/kaggle/working/CS431.P22/datasets'

5a7f0b841c517d119f5f64726ae75ef6eae87689
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-01 23:43:04 +0700

    update

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -209,7 +209,7 @@ def extract_and_normalize_document_key(citation_text):
         number = match2.group(1)
         year = match2.group(2)
         identifier = match2.group(3)
-        if identifier == 'QH':
+        if identifier == 'qh':
             identifier == 'QH15'
         key = f"{number}_{year}_{identifier}".upper()
         st.write(key)

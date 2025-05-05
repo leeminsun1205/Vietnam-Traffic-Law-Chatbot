a26941288fa8baae7a3651dd4ca679d76d9a616a
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-05 18:25:40 +0700

    update

--- chatbot/retriever.py
+++ chatbot/retriever.py
@@ -136,6 +136,7 @@ class HybridRetriever:
             logging.debug(f"Performing DENSE search for: '{query_text[:50]}...' with k={k}")
             distances, indices = retrieve_relevant_chunks(query_text, embedding_model, self.vector_db, k=k)
             st.write(distances, indices)
+            st.write(indices.type)
             if indices is not None and len(indices) > 0:
                 for i, idx in enumerate(indices):
                      # Đảm bảo idx hợp lệ

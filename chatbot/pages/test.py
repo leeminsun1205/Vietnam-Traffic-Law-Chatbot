98aaedf26ba3cf3aa2c041a5660c257de79f14f6
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-05 08:51:05 +0700

    update

--- chatbot/pages/test.py
+++ chatbot/pages/test.py
@@ -1,7 +1,7 @@
 # app.py (Nội dung tạm thời để test)
 import streamlit as st
 
-st.set_page_config(page_title="Test MultiPage") # Chỉ gọi 1 lần ở đây
+# st.set_page_config(page_title="Test MultiPage") # Chỉ gọi 1 lần ở đây
 st.title("Trang Chính Tối Giản")
 st.write("Đây là nội dung trang chính.")
 

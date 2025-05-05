e6db0c3912a35d52c7e4bf9e10951195edd1d771
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-05 17:54:40 +0700

    update

--- chatbot/vector_db.py
+++ chatbot/vector_db.py
@@ -40,6 +40,7 @@ class SimpleVectorDatabase:
         st.write('END')
         query_embedding_array = np.array([query_embedding]).astype('float32')
         st.write(query_embedding_array)
+        st.write('bbbbb')
         if query_embedding_array.shape[1] != self.embedding_dimension:
             st.write('CHECK')
             return [], []

7a95d646331caae61e01537ce48e1edce2f72b64
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-05 10:11:33 +0700

    update

--- chatbot/pages/Evaluation.py
+++ chatbot/pages/Evaluation.py
@@ -129,7 +129,7 @@ def run_retrieval_evaluation(
             query_metrics["summarizing_query"] = summarizing_query
             query_metrics["num_variations_generated"] = len(all_queries)
             # st.write(relevance_status) # (Bỏ các st.write không cần thiết)
-            # st.write(all_queries)
+            st.write(all_queries)
             # st.write(summarizing_query)
             if relevance_status == 'invalid':
                 query_metrics["status"] = "skipped_irrelevant"

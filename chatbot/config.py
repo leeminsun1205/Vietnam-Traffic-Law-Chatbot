f4f93ccc5fa45ab7416f9dff3f81984862e991b1
leeminsun1205 <minhnhut12052004@gmai.com>
2025-05-19 21:41:00 +0700

    update datasets

--- chatbot/config.py
+++ chatbot/config.py
@@ -28,8 +28,10 @@ TRAFFIC_SIGN_IMAGES_ROOT_DIR = '/kaggle/working/CS431.P22/traffic_sign'
 JSON_DATA_PATH = KAGGLE_INPUT_PATH 
 JSON_FILE_PATTERN = os.path.join(JSON_DATA_PATH, 'legal_{i}.json')
 NUM_FILES = 51 
-NUMBERS_TO_SKIP = {29, 30, 37, 38, 39, 40}
-
+# NUMBERS_TO_SKIP = {29, 30, 37, 38, 39, 40}
+NUMBERS_TO_SKIP = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                   21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                   41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 MAX_HISTORY_TURNS = 10
 
 DEFAULT_MAX_LEN = 512 
